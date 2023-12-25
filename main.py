269e7ab81f36c33ecd0ace044387b9d7f17d26ef
chrislavallee@gmail.com <chrislavallee@gmail.com>
2023-12-25 16:05:42 -0600

    Hello, world

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-def main() -> None:
-    name = input("What's your name? ")
-    print(f"Hello, {name}!")
-    print("Nice to meet you!")
-
-
 if __name__ == "__main__":
-    main()
+    print("Helo, world!")
